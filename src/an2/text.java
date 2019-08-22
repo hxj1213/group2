3113e95edcc3847c3c6da49bb522e11701a13f8a
gbq <you@example.com>
2019-08-22 15:21:37 +0800

    wo dangzushangle

--- src/an2/text.java
+++ src/an2/text.java
@@ -3,5 +3,33 @@ package an2;
 public class text {
     public static void main(String[] args) {
         System.out.println("an is pig");
+
+    }
+
+    public class id{
+             private int i;
+             private String neme;
+             private String max;
+
+        public id(int i) {
+            this.i = i;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+
+        @Override
+        public String toString() {
+            return "id{" +
+                    "i=" + i +
+                    ", neme='" + neme + '\'' +
+                    ", max='" + max + '\'' +
+                    '}';
+        }
     }
 }
